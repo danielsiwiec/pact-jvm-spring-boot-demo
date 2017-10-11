cd5d25799b867f2cdcc38cb008695bbe53b80eef
Dan Siwiec <daniel.siwiec@gmail.com>
2017-10-11 16:00:08 -0700

    Create a simple pact implementation

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pact JVM Spring Boot demo
+
+## Running
+
+Open two tabs:
+
+#### First tab
+Run `./gradlew :provider:bootRun`
+
+#### Second tab
+Run `./gradlew pactVerify`
--- provider/src/main/java/com/dansiwiec/Controller.java
+++ provider/src/main/java/com/dansiwiec/Controller.java
@@ -1,5 +1,6 @@
 package com.dansiwiec;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,8 @@ public class Controller {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/greeting")
-	public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
-		return String.format(template, name);
+	@RequestMapping(value = "/person/{id}", produces = "application/json")
+	public Person getPerson(@PathVariable("id") Long id) {
+		return new Person(1L, "John");
 	}
 }
